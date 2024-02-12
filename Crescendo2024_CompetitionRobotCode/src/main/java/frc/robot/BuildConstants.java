68de0d925aaa474eb812ccc15c4ea449497851be
Kynam L <115427320+Noto501@users.noreply.github.com>
2024-02-11 16:16:26 -0800

    Fixed Intake CAN ID - LC and DF
    Fix
    - changed pivot motor and intake roller motor CAN ID

--- Crescendo2024_CompetitionRobotCode/src/main/java/frc/robot/BuildConstants.java
+++ Crescendo2024_CompetitionRobotCode/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo2024_CompetitionRobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 45;
-  public static final String GIT_SHA = "1d2469b9938d143aa59881f151f99898214b3ed6";
-  public static final String GIT_DATE = "2024-02-09 22:52:31 EST";
+  public static final int GIT_REVISION = 46;
+  public static final String GIT_SHA = "b6b5ef7f6873e610817c3fc6808c0bde8274b7f3";
+  public static final String GIT_DATE = "2024-02-10 19:24:04 EST";
   public static final String GIT_BRANCH = "Mechanism-branch";
-  public static final String BUILD_DATE = "2024-02-10 19:19:48 EST";
-  public static final long BUILD_UNIX_TIME = 1707610788879L;
+  public static final String BUILD_DATE = "2024-02-11 18:46:51 EST";
+  public static final long BUILD_UNIX_TIME = 1707695211838L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
