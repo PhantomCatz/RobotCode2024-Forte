4baf8c86172529727c1f616c0aeb8e63ef37072e
William <149019293+IPutSoapInMyEyes@users.noreply.github.com>
2024-02-19 12:32:31 -0800

    l

--- Crescendo2024_CompetitionRobotCode/src/main/java/frc/robot/BuildConstants.java
+++ Crescendo2024_CompetitionRobotCode/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Crescendo2024_CompetitionRobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 41;
-  public static final String GIT_SHA = "8ee367f512b088b5796aa7b18a329abaec4d5bf0";
-  public static final String GIT_DATE = "2024-02-17 18:52:44 EST";
+  public static final int GIT_REVISION = 42;
+  public static final String GIT_SHA = "06fefc940a111121506d9fe955c18817fa6246ce";
+  public static final String GIT_DATE = "2024-02-19 15:31:32 EST";
   public static final String GIT_BRANCH = "shootersunday";
-  public static final String BUILD_DATE = "2024-02-19 15:01:58 EST";
-  public static final long BUILD_UNIX_TIME = 1708372918469L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-02-19 15:32:13 EST";
+  public static final long BUILD_UNIX_TIME = 1708374733169L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
