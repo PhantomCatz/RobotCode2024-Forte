acc881b14472c63b18a10491e518bd8470ca29ab
Kynam L <115427320+Noto501@users.noreply.github.com>
2024-02-28 19:05:25 -0800

    added servo and other logic and renamed stuff- WW

--- Crescendo2024_CompetitionRobotCode/src/main/java/frc/robot/RobotContainer.java
+++ Crescendo2024_CompetitionRobotCode/src/main/java/frc/robot/RobotContainer.java
@@ -1,24 +1,11 @@
 package frc.robot;
 
-import com.pathplanner.lib.auto.NamedCommands;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.CatzConstants.OIConstants;
-import frc.robot.commands.DriveCmds.TeleopDriveCmd;
-import frc.robot.commands.StateMachineCmds.MoveToNewPositionCmd;
-import frc.robot.subsystems.drivetrain.SubsystemCatzDrivetrain;
 import frc.robot.subsystems.elevator.SubsystemCatzElevator;
-import frc.robot.subsystems.intake.SubsystemCatzIntake;
 import frc.robot.subsystems.shooter.SubsystemCatzShooter;
 import frc.robot.subsystems.vision.SubsystemCatzVision;
 
@@ -99,8 +86,8 @@ import frc.robot.subsystems.vision.SubsystemCatzVision;
     xboxDrv.leftTrigger().onTrue(shooter.loadBackward());
     xboxDrv.leftBumper().onTrue(shooter.setPosition(0.25));
     //xboxDrv.rightStick().onTrue(shooter.setPosition(xboxDrv.getRightY()));
-
     xboxDrv.rightBumper().onTrue(shooter.cmdLoad());
+    
 
   }
 
