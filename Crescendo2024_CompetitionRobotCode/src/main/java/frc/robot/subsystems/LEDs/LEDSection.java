c15e7777caa731528b9a9f8e2bb5e6827a4a6219
Kynam L <117879892+Hyoungjun07@users.noreply.github.com>
2024-03-30 11:51:20 -0700

    Feature/led code
    visual studio code is not showing me errors pls fix this

--- Crescendo2024_CompetitionRobotCode/src/main/java/frc/robot/subsystems/LEDs/LEDSection.java
+++ Crescendo2024_CompetitionRobotCode/src/main/java/frc/robot/subsystems/LEDs/LEDSection.java
@@ -6,115 +6,63 @@ public class LEDSection {
     public enum LEDMode {
         Solid,
         Blink,
-        Alternating,
-        Flow;
+        Alternate,
     }
 
-    
-
     private final int LED_COUNT;
-    private int iteration;
-    private int currentIteration = 0;
-    private int alt = 0;
+    private final double BLINK_FREQ = 2;
 
-    private Color[] ledColors;
-    private Color[] ledColors2;
-    private LEDMode ledMode = LEDMode.Solid;
+    private Color[] baseColors;
+    private Timer timer;
+    public LEDMode ledMode = LEDMode.Solid;
 
     public LEDSection(int ledCount) {
         LED_COUNT = ledCount;
-        ledColors = new Color[LED_COUNT];
-        ledColors2 = new Color[LED_COUNT];
+        baseColors = new Color[LED_COUNT];
+        timer.reset();
+        timer.start();
     }
 
-    public void setIteration(int iteration) {
-        this.iteration = iteration;
-    }
-
-    public void setMode(LEDMode mode) {
-        ledMode = mode;
+    private boolean canBlink(){
+        return (int)(timer.get() * BLINK_FREQ)%2 == 0
     }
 
     public void colorSolid(Color color) {
         for (int i = 0; i < LED_COUNT; i++) {
-            ledColors[i] = color;
+            baseColors[i] = color;
         }
     }
 
-    public void colorAlternating(Color color1, Color color2) {
-        for (int i = 0; i < LED_COUNT; i++) {
-            if (i % 2 == 0) {
-                ledColors[i] = color1;
+    public void colorAlternating(Color color, Color back){
+        for(int i=0; i<LED_COUNT; i++){
+            if(i%2 == 0){
+                baseColors[i] = back;
             } else {
-                ledColors[i] = color2;
+                baseColors[i] = color;
             }
-        }
-    }
-
-    public void colorRainbow() {
-        for (int i = 0; i < LED_COUNT; i++) {
-            ledColors[i] = Color.fromHSV((int) ((double) i / LED_COUNT * 180), 255, 255);
-        }
-    }
-
-    public void colorSolid2(Color color) {
-        for (int i = 0; i < LED_COUNT; i++) {
-            ledColors2[i] = color;
-        }
-    }
-
-    public void colorAlternating2(Color color1, Color color2) {
-        for (int i = 0; i < LED_COUNT; i++) {
-            if (i % 2 == 0) {
-                ledColors2[i] = color1;
-            } else {
-                ledColors2[i] = color2;
-            }
-        }
-    }
-
-    public void colorRainbow2() {
-        for (int i = 0; i < LED_COUNT; i++) {
-            ledColors2[i] = Color.fromHSV((int) ((double) i / LED_COUNT * 180), 255, 255);
         }
     }
 
     public Color[] getColors() {
-        currentIteration++;
-
-        if (currentIteration >= iteration) {
-            currentIteration = 1;
-
-            if (alt > 200000) {
-                alt = 1;
+        Color[] currentColors = new Color[LED_COUNT];
+        for(int i=0; i<LED_COUNT; i++){
+            if(LEDMode = enum.LEDMode.Solid){
+                colors[i] = ledColors[i];
             }
-
-            alt++;
-            
-            switch (ledMode) {
-                case Solid:
-                    return ledColors;
-
-                case Blink:
-                    if (alt % 2 == 0) {
-                        return ledColors;
-                    } else {
-                        return new Color[LED_COUNT];
-                    }
-
-                case Alternating:
-                    if (alt % 2 == 0) {
-                        return ledColors;
-                    } else {
-                        return ledColors2;
-                    }
-
-                case Flow:
-                    for (int i = 0; i < LED_COUNT; i++) {
-                        ledColors[i] = ledColors[(i + 1) % LED_COUNT];
-                    }
-                    return ledColors;
-            }    
+            if(LEDMode = enum.LEDMode.Blink){
+                if(canBlink()){
+                    colors[i] = Color.kBlack;
+                } else {
+                    colors[i] = ledColors[i];
+                }
+            }
+            if(LEDMode == enum.LEDMode.Alternate){
+                if(canBlink() && i%2 == 0){
+                    colors[i] = Color.kBlack;
+                } else {
+                    colors[i] = ledColors[i];
+                }
+            }
         }
         return ledColors;
     }
